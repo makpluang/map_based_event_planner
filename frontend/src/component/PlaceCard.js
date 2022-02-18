5830e23b80f9d056162aa3166d8be3db4d0d2f35
Sonali <sonali.singh@pluang.com>
2022-02-18 14:01:07 +0530

    Map dummy places data to places card

--- frontend/src/component/PlaceCard.js
+++ frontend/src/component/PlaceCard.js
@@ -3,28 +3,28 @@ import { Card, CardBody, CardImg, CardTitle, CardText } from "reactstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-const PlaceCard = () => {
+const PlaceCard = ({place}) => {
     return (
         <Card className="place-card">
             <CardBody>
                 <CardImg
                     alt="Card image cap"
-                    src="https://picsum.photos/318/180"
+                    src={place.image}
                     top
                     width="100%"
                     className="place-card-img"
                 />
                 <CardTitle tag="h6" className="mt-3 mb-2 d-flex justify-content-between">
-                    Card title
+                    {place.title}
                     <div className="rating">
                     <span className="rating-icon">
                         <FontAwesomeIcon className="rating-icon-font" icon={faStar} />
                     </span>
-                        5.0
+                    {place.rating}
                     </div>
                 </CardTitle>
                 <CardText className="text-muted">
-                    This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                    {place.about}
                 </CardText>
             </CardBody>
         </Card>
--- frontend/src/layout/Places.js
+++ frontend/src/layout/Places.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Card, CardBody, CardTitle} from "reactstrap"
 
+import placesData from "../data"
+
 import PlaceCard from "../component/PlaceCard";
 
 const Places = () => {
     return (
-        <Card title="Route">
+        <Card className="place-column">
             <CardBody>
                 <CardTitle tag="h5">
                     Upcoming Places
                 </CardTitle>
-              < PlaceCard/>
+               { placesData.route.map((place) => 
+                    < PlaceCard place={place} key={place.id} />)
+                }
+              
             </CardBody>
         </Card>
     )
